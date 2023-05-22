134f515adaa992dabb1abb55373af0ebf7395af2
Andrey Fedorov <fvandre@outlook.com>
2023-05-22 15:44:38 +0300

    Add dependencies and tests for API JUnit Jupiter

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -41,4 +41,44 @@ public class CashbackHackServiceTest {
 
         org.junit.Assert.assertEquals(0, service.remain(1000));
     }
+
+    @org.junit.jupiter.api.Test
+    public void shouldCalculateRemain1() {
+
+        CashbackHackService service = new CashbackHackService();
+
+        org.junit.Assert.assertEquals(500, service.remain(500));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void shouldCalculateRemainIfAmountIsZero1() {
+
+        CashbackHackService service = new CashbackHackService();
+
+        org.junit.Assert.assertEquals(1000, service.remain(0));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void shouldCalculateRemainIfAmountIsOne1() {
+
+        CashbackHackService service = new CashbackHackService();
+
+        org.junit.Assert.assertEquals(999, service.remain(1));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void shouldCalculateRemainIfAmountLessOneThanThousand1() {
+
+        CashbackHackService service = new CashbackHackService();
+
+        org.junit.Assert.assertEquals(1, service.remain(999));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void shouldCalculateRemainIfAmountEqualsThousand1() {
+
+        CashbackHackService service = new CashbackHackService();
+
+        org.junit.Assert.assertEquals(0, service.remain(1000));
+    }
 }
